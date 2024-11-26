a594c69a639c3b5f873fb03435ac97be07d57749
PRATHAM <6230809462game@gmail.com>
2024-11-26 19:04:48 +0530

    practice

--- class10.py
+++ class10.py
@@ -1,6 +1,71 @@
-number =int(input("enter a number"))
-if number%2 == 0:
-    print("Even")
-else:
-    print("odd")
+# number =int(input("enter a number"))
+# if number%2 == 0:
+#     print("Even")
+# else:
+#     print("odd")
    
+# year = int(input("Enter a year: "))
+
+# if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+#     print("{year} is a leap year.")
+# else:
+#     print("{year} is not a leap year.")
+
+
+# iterate from i = 0 to i = 3
+# for a in range(100):
+#     print(a)
+
+# for i in range(3,21):
+#     print(i)
+# if i %2 == 0:
+    # print(i))
+    
+    
+# print("Table for 5:")
+
+
+
+
+# for i in range(0, 11):
+#     print(f"5 x {i} = {5 * i}")
+
+
+
+
+
+
+
+
+# for i in range(0,21):
+#     if i%2==0:
+#         print(i)
+# for i in range(0,11):
+#      print(i*2)
+
+
+
+# for i in range(0,33,3):
+#     print(i)
+
+
+# number= int(input("enter any number"))
+# for i in range(0,11):
+#     print(i*7)
+    
+    
+# for i in range(3,20,4):
+#         print(i)
+
+
+
+
+
+
+# for i in range(20):  
+#     if 1 <= i <= 13:  
+#         print(i)
+
+for i in range(20):
+ if 1 <= i <=20 and i !=12 and i !=17:
+             print(i)
